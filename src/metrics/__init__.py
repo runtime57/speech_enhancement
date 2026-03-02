d8b3ed146401d2eaddc861c28ec18d029f935d57
runtime57 <runtime57@yandex.ru>
2026-03-02 18:14:09 +0300

    Add metrics, fix VCTK

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -2,4 +2,5 @@ from src.metrics.example import ExampleMetric
 from src.metrics.pesq import PESQ
 from src.metrics.sdri import SDRi
 from src.metrics.si_snri import SI_SNRi
+from src.metrics.si_snr import SI_SNR
 from src.metrics.stoi import STOI
---
+++ src/metrics/si_snr.py
@@ -0,0 +1,23 @@
+import torch
+from torchmetrics.audio.snr import ScaleInvariantSignalNoiseRatio
+
+from src.metrics.base_metric import BaseMetric
+
+
+class SI_SNR(BaseMetric):
+    """
+    Scale-Invariant Signal-to-Noise Ratio Improvement metric.
+    Higher is better.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.metric = ScaleInvariantSignalNoiseRatio().to(self.device)
+
+    def __call__(self, separated_audio, audio_target, audio_mix, **kwargs):
+        if separated_audio.dim() == 2:
+            separated_audio = separated_audio.unsqueeze(1)
+        if audio_target.dim() == 2:
+            audio_target = audio_target.unsqueeze(1)
+        si_snr_separated = self.metric(separated_audio, audio_target)
+        return torch.mean(si_snr_separated).item()
