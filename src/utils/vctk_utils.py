8b509ade10878642f83560dc924537efc9f1671a
runtime57 <runtimeerror57@gmail.com>
2026-03-02 22:05:34 +0300

    Fix split creation

--- src/utils/vctk_utils.py
+++ src/utils/vctk_utils.py
@@ -4,13 +4,13 @@ from csv import DictReader
 from random import shuffle
 from pathlib import Path
 
-def get_flac_paths(directory='.'):
+def get_wav_paths(directory='.'):
     start_path = Path(directory)
-    flac_list = [
+    wav_list = [
         str(file.absolute())
-        for file in start_path.rglob('*.flac')
+        for file in start_path.rglob('*.wav')
     ]
-    return list(sorted(flac_list))
+    return list(sorted(wav_list))
 
 def create_vctk_split(split_name, rawdata_path, dataset_path):
     assert split_name in ["train", "test"]
@@ -18,8 +18,8 @@ def create_vctk_split(split_name, rawdata_path, dataset_path):
     rawdata_path = Path(rawdata_path)
     dataset_path = Path(dataset_path)
 
-    clean_paths = get_flac_paths(rawdata_path / f"clean_{split_name}set_wav")
-    noisy_paths = get_flac_paths(rawdata_path / f"noisy_{split_name}set_wav")
+    clean_paths = get_wav_paths(rawdata_path / f"clean_{split_name}set_wav")
+    noisy_paths = get_wav_paths(rawdata_path / f"noisy_{split_name}set_wav")
 
     split = []
     for clean, noisy in zip(clean_paths, noisy_paths):
